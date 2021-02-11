d857987845799b477b0d602c51027aef8300896a
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2021-02-11 22:04:44 +0200

    dimaarion/basketboll.git

--- sketch.js
+++ sketch.js
@@ -1,26 +1,283 @@
 /* eslint-disable no-undef, no-unused-vars */
-let s = 0;
-let h = 60;
-let count = 0;
-let cdr = [];
-let value = 0;
-let speed = 0;
-let levels = 0;
-let basketInH = 0;
-let b = 0;
-let basketShar = [{}];
-let speedShar, sharX, sharY;
-let counrShar = 0;
-var hit = false;
+
+const basketGame = {
+  sittens: {
+    speedKadr: {
+      min: 0,
+      max: 8,
+      count: { min: 0, max: 1 }
+    },
+    keyboard: {
+      boardPressLeftRight: {
+        speed: {
+          min: 0,
+          max: 3
+        },
+        value: 0
+      }
+    },
+    windowGame: {
+      right: 150,
+      left: 0,
+      rightPanel: 20
+    }
+  },
+  basketShar: [{}],
+  basketCar: {
+    wheels: {
+      circle1X: 30,
+      circle1Y: 60,
+      circle2X: 125,
+      circle2Y: 60,
+      circleRad: 20,
+      strokeColor: "green",
+      fill: 225
+    },
+    body: {
+      rect1X: 10,
+      rect1Y: 110,
+      rectWidth1: 100,
+      rectHeight1: 50,
+      rect2X: 110,
+      rect2Y: 90,
+      rectWidth2: 30,
+      rectHeight2: 30,
+      rect3X: 110,
+      rect3Y: 105,
+      rectWidth3: 20,
+      rectHeight3: 20,
+      rectCabX: 112,
+      rectCabY: 103,
+      rectWidthCab: 16,
+      rectHeightCab: 13,
+      fillCab: 255,
+      line1X: 5,
+      line1Y: 120,
+      line1aX: 18,
+      line1aY: 105,
+      line2X: 112,
+      line2Y: 120,
+      line2aX: 101,
+      line2aY: 105,
+      line3X: 125,
+      line3Y: 120,
+      line3aX: 112,
+      line3aY: 120,
+      line4X: 15,
+      line4Y: 110,
+      line4aX: 101,
+      line4aY: 110,
+      text: {}
+    }
+  },
+  basketFooter: {
+    rectX: 0,
+    rectY: 50,
+    rectHeight: 30
+  },
+  pole(height, width) {
+    fill("green");
+    rect(
+      this.basketFooter.rectX,
+      height - this.basketFooter.rectY,
+      width,
+      this.basketFooter.rectHeight
+    );
+  },
+  startGame(height) {
+    this.sittens.speedKadr.min =
+      this.sittens.speedKadr.min + this.sittens.speedKadr.max;
+    if (this.sittens.speedKadr.min > height - this.basketShar[0].radShar) {
+      this.sittens.speedKadr.min = 0;
+      this.sittens.speedKadr.count.min =
+        this.sittens.speedKadr.count.min + this.sittens.speedKadr.count.max;
+    }
+  },
+  controlGame(width) {
+    if (this.sittens.keyboard.boardPressLeftRight.value === 2) {
+      this.sittens.keyboard.boardPressLeftRight.speed.min =
+        this.sittens.keyboard.boardPressLeftRight.speed.min +
+        this.sittens.keyboard.boardPressLeftRight.speed.max;
+    }
+    if (this.sittens.keyboard.boardPressLeftRight.value === 1) {
+      this.sittens.keyboard.boardPressLeftRight.speed.min =
+        this.sittens.keyboard.boardPressLeftRight.speed.min -
+        this.sittens.keyboard.boardPressLeftRight.speed.max;
+    }
+    if (
+      this.sittens.keyboard.boardPressLeftRight.speed.min <
+      this.sittens.windowGame.left
+    ) {
+      this.sittens.keyboard.boardPressLeftRight.speed.min = this.sittens.windowGame.left;
+    }
+    if (
+      this.sittens.keyboard.boardPressLeftRight.speed.min >
+      width - this.sittens.windowGame.right
+    ) {
+      this.sittens.keyboard.boardPressLeftRight.speed.min =
+        width - this.sittens.windowGame.right;
+    }
+  },
+  controlShar(height, width) {
+    let hit = false;
+    let hit2 = false;
+    let hitL = false;
+    let hitL2 = false;
+    let hitC = false;
+    let hitC1 = false;
+    let speed = this.sittens.keyboard.boardPressLeftRight.speed.min;
+    for (let i = 0; i < this.sittens.speedKadr.count.min; i++) {
+      this.basketShar[i].sharY += this.basketShar[i].speedShar;
+      fill(
+        this.basketShar[i].colorShar1,
+        this.basketShar[i].colorShar2,
+        this.basketShar[i].colorShar3
+      );
+      circle(
+        this.basketShar[i].sharX,
+        this.basketShar[i].sharY,
+        this.basketShar[i].radShar
+      );
+
+      hit = collideRectCircle(
+        0,
+        height - 50,
+        width,
+        50,
+        this.basketShar[i].sharX,
+        this.basketShar[i].sharY,
+        this.basketShar[i].radShar
+      );
+      hit2 = collideRectCircle(
+        this.basketCar.body.rect2X + speed,
+        height - this.basketCar.body.rect2Y,
+        this.basketCar.body.rectWidth2,
+        this.basketCar.body.rectHeight2,
+        this.basketShar[i].sharX,
+        this.basketShar[i].sharY,
+        this.basketShar[i].radShar
+      );
+      hitL = collideLineCircle(
+        15 + speed,
+        height - 110,
+        101 + speed,
+        height - 110,
+        this.basketShar[i].sharX,
+        this.basketShar[i].sharY,
+        this.basketShar[i].radShar
+      );
+      hitL2 = collideLineCircle(
+        5 + speed,
+        height - 120,
+        18 + speed,
+        height - 105,
+        this.basketShar[i].sharX,
+        this.basketShar[i].sharY,
+        this.basketShar[i].radShar
+      );
+      hitC = collideCircleCircle(
+        this.basketShar[i].sharX,
+        this.basketShar[i].sharY,
+        this.basketShar[i].radShar,
+        this.basketCar.wheels.circle1X + speed,
+        height - this.basketCar.wheels.circle1Y,
+        this.basketCar.wheels.circleRad
+      );
+      if (hit) {
+        this.basketShar[i].sharY += -1;
+      }
+      if (hit2 || hitC) {
+        this.basketShar[i].sharY -= 100;
+      }
+      if (hitL || hitL2) {
+        this.basketShar[i].sharX = 60 + speed;
+        this.basketShar[i].sharY = height - 120;
+        this.basketShar[i].radShar = 30 + i;
+      }
+    }
+  },
+  basket(height, speed) {
+    fill("blue");
+    rect(
+      this.basketCar.body.rect1X + speed,
+      height - this.basketCar.body.rect1Y,
+      this.basketCar.body.rectWidth1,
+      this.basketCar.body.rectHeight1
+    );
+    noStroke();
+    rect(
+      this.basketCar.body.rect2X + speed,
+      height - this.basketCar.body.rect2Y,
+      this.basketCar.body.rectWidth2,
+      this.basketCar.body.rectHeight2
+    );
+    rect(
+      this.basketCar.body.rect3X + speed,
+      height - this.basketCar.body.rect3Y,
+      this.basketCar.body.rectWidth3,
+      this.basketCar.body.rectHeight3
+    );
+    fill(this.basketCar.body.fillCab);
+    rect(
+      this.basketCar.body.rectCabX + speed,
+      height - this.basketCar.body.rectCabY,
+      this.basketCar.body.rectWidthCab,
+      this.basketCar.body.rectHeightCab
+    );
+    //Колеса
+    fill(this.basketCar.wheels.fill);
+    stroke(this.basketCar.wheels.strokeColor);
+    circle(
+      this.basketCar.wheels.circle1X + speed,
+      height - this.basketCar.wheels.circle1Y,
+      this.basketCar.wheels.circleRad
+    );
+    circle(
+      this.basketCar.wheels.circle2X + speed,
+      height - this.basketCar.wheels.circle2Y,
+      this.basketCar.wheels.circleRad
+    );
+
+    stroke("blue");
+    strokeWeight(10);
+    line(
+      this.basketCar.body.line1X + speed,
+      height - this.basketCar.body.line1Y,
+      this.basketCar.body.line1aX + speed,
+      height - this.basketCar.body.line1aY
+    );
+    line(
+      this.basketCar.body.line1X + speed,
+      height - this.basketCar.body.line1Y,
+      this.basketCar.body.line1aX + speed,
+      height - this.basketCar.body.line1aY
+    );
+    line(
+      this.basketCar.body.line3X + speed,
+      height - this.basketCar.body.line3Y,
+      this.basketCar.body.line3aX + speed,
+      height - this.basketCar.body.line3aY
+    );
+    line(
+      this.basketCar.body.line4X + speed,
+      height - this.basketCar.body.line4Y,
+      this.basketCar.body.line4aX + speed,
+      height - this.basketCar.body.line4aY
+    );
+    strokeWeight(1);
+  }
+};
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(
+    windowWidth - basketGame.sittens.windowGame.rightPanel,
+    windowHeight
+  );
   for (let j = 0; j < windowWidth; j++) {
-    cdr[j] = random(10, windowWidth - 10);
-    basketShar[j] = {
+    basketGame.basketShar[j] = {
       colorShar1: random(0, 255),
       colorShar2: random(0, 255),
       colorShar3: random(0, 255),
-      sharX: random(10, windowWidth - 10),
+      sharX: random(10, windowWidth - 30),
       sharY: 0,
       radShar: 30,
       speedShar: 1
@@ -30,197 +287,31 @@ function setup() {
 }
 
 function draw() {
-  let stopW = 150;
-  let speedP = 3 + levels;
-  let speedH = 3 + levels;
   background(255);
   fill("red");
   stroke(255);
-
-  //basketShar[0].speedShar += basketShar[0].speedShar;
-  s = s + 8;
-  if (s > windowHeight - basketShar[0].radShar) {
-    s = 0;
-    count = count + 1;
-  }
-
-  if (value === 2) {
-    speed = speed + speedP;
-  }
-  if (value === 1) {
-    speed = speed - speedP;
-  }
-  if (speed < 0) {
-    speed = 0;
-  }
-  if (speed > windowWidth - stopW) {
-    speed = windowWidth - stopW;
-  }
-  pole(windowHeight);
-
-  for (let i = 0; i < count; i++) {
-    basketShar[i].sharY += basketShar[i].speedShar;
-    fill(
-      basketShar[i].colorShar1,
-      basketShar[i].colorShar2,
-      basketShar[i].colorShar3
-    );
-    circle(basketShar[i].sharX, basketShar[i].sharY, basketShar[i].radShar);
-
-    hit = collideRectCircle(
-      0,
-      windowHeight - 50,
-      windowWidth,
-      50,
-      basketShar[i].sharX,
-      basketShar[i].sharY,
-      basketShar[i].radShar
-    );
-    hit2 = collideRectCircle(
-      110 + speed,
-      windowHeight - 90,
-      30,
-      30,
-      basketShar[i].sharX,
-      basketShar[i].sharY,
-      basketShar[i].radShar
-    );
-    hitL = collideLineCircle(
-      15 + speed,
-      windowHeight - 110,
-      101 + speed,
-      windowHeight - 110,
-      basketShar[i].sharX,
-      basketShar[i].sharY,
-      basketShar[i].radShar
-    );
-    hitL2 = collideLineCircle(
-      5 + speed,
-      windowHeight - 120,
-      18 + speed,
-      windowHeight - 105,
-      basketShar[i].sharX,
-      basketShar[i].sharY,
-      basketShar[i].radShar
-    );
-    if (hit) {
-      //circle(basketShar[i].sharX, (basketShar[i].sharY *= -1), 100);
-      basketShar[i].sharY += -1;
-    }
-    if (hit2) {
-      basketShar[i].sharY -= 150;
-    }
-    if (hitL || hitL2) {
-      basketShar[i].sharX = 60 + speed;
-      basketShar[i].sharY = windowHeight - 120;
-      basketShar[i].radShar = 30 + i;
-      if (i > 0) {
-        //basketShar[i - 1].sharX = 50 + speed;
-      }
-      if (i > 1) {
-        //basketShar[i - 2].sharX = 80 + speed;
-        // basketShar[i].sharY = windowHeight - 120;
-      }
-    }
-  }
-
-  // cricleses(cdr, count);
-  basket(windowHeight);
-  //rezultTextBascet(colize(cdr[count], speed, s, windowHeight, count));
-  //bumCircles(cdr, count, windowHeight, windowWidth);
+  basketGame.startGame(windowHeight);
+  basketGame.controlGame(windowWidth);
+  basketGame.pole(windowHeight, windowWidth);
+  basketGame.controlShar(windowHeight, windowWidth);
+  basketGame.basket(
+    windowHeight,
+    basketGame.sittens.keyboard.boardPressLeftRight.speed.min
+  );
 }
+
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    value = 1;
+    basketGame.sittens.keyboard.boardPressLeftRight.value = 1;
   } else if (keyCode === RIGHT_ARROW) {
-    value = 2;
-  }
-}
-function cricleses(arr, x) {
-  fill("green");
-  circle(arr[x], s, 20);
-}
-
-function pole(w) {
-  for (let i = 0; i < 50; i++) {
-    fill("green");
-    rect(100 * i, w - 50, 100, 30);
-  }
-}
-
-function basket(hw) {
-  let w = 100;
-  let h = 50;
-  let kw = 30;
-  let kh = 30;
-  fill("blue");
-  rect(10 + speed, hw - 110, w, h);
-  noStroke();
-  rect(w + 10 + speed, hw - 90, kw, kh);
-  rect(w + 10 + speed, hw - 105, 20, 20);
-  fill(225);
-  rect(w + 12 + speed, hw - 103, 16, 13);
-  fill(210);
-  stroke("green");
-  circle(30 + speed, hw - 60, 20);
-  circle(125 + speed, hw - 60, 20);
-
-  stroke("blue");
-  strokeWeight(10);
-  line(5 + speed, hw - 120, 18 + speed, hw - 105);
-  line(112 + speed, hw - 120, 101 + speed, hw - 105);
-  line(125 + speed, hw - 120, 112 + speed, hw - 120);
-  line(15 + speed, hw - 110, 101 + speed, hw - 110);
-  strokeWeight(1);
-}
-
-function colize(hY, kX, hX, windH) {
-  let kXMax = kX + 150;
-  let hYc = round(hY);
-  if (hYc > kX && hYc < kXMax && hX > windH - 110) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function rezultTextBascet(x, bumC) {
-  let cri = 60;
-  let criR = 50;
-  let criX = 43;
-  let criY = 70;
-
-  if (x === true) {
-    for (let j = 0; j < 100; j++) {
-      cdr[j] = random(10, windowWidth - 10);
-    }
-    s = 0;
-    basketInH = basketInH + 1;
-  } else {
-  }
-
-  for (let i = 0; i < basketInH; i++) {
-    fill(119, 221, 231);
-    circle(cri + cri * i, cri, criR);
-    textSize(30);
-    fill("red");
-    if (i > 8) {
-      text(" " + (i + 1) + " ", criX - 9 + cri * i, criY);
-    } else {
-      text(" " + (i + 1) + " ", criX + cri * i, criY);
-    }
-  }
-}
-
-function bumCircles(sv, c, h, w) {
-  b = b + 1;
-  fill("green");
-  for (let i = 0; i < 10; i++) {
-    //circle(sv[c - 1], h - (60 + b), 10);
+    basketGame.sittens.keyboard.boardPressLeftRight.value = 2;
   }
 }
 
 // This Redraws the Canvas when resized
 windowResized = function () {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(
+    windowWidth - basketGame.sittens.windowGame.rightPanel,
+    windowHeight
+  );
 };
